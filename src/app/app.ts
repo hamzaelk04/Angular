dc0abd0f1f0884b6451dd282c0e43c68f7f629c1
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-05-19 22:25:19 +0100

    test for user card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserCard } from "./user-card/user-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserCard],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-app');
+  count = 0;
+  title = "";
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+
+  limit() {
+    if (this.count <= 0) {
+      this.title = "You have reached the limit";                                                                                                            
+    }
+  }
+
+  onDecrement() {
+    this.decrement();
+
+    this.limit();
+  }
 }
